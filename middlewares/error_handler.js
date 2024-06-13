a8d4a8b562a9e212297301a18b34769a44f65401
Daniel Asaboro <asaborodaniel@gmail.com>
2024-06-13 23:50:30 +0100

    feat: added custom error handler

--- middlewares/error_handler.js
+++ middlewares/error_handler.js
@@ -1,1 +1,21 @@
 // app custom error handler
+
+const errorHandler = (error) => {
+    let errorObject = {};
+    console.log('we got here');
+    if (error.message.includes('User validation failed')) {
+        const errorValues = Object.values(error.errors);
+        errorValues.forEach(element => {
+            errorObject[element.path] = element.message;
+        });
+    }
+    if (error.code == '11000') {
+        errorObject.email = `User with email "${error.keyValue.email}" already exist!`;
+    }
+
+    console.log(errorObject)
+
+    return errorObject;
+}
+
+export default errorHandler;
