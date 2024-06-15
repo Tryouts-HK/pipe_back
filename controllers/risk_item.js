dcf63b7dac3045b30bab8f8c41de2c4663055df3
Daniel Asaboro <asaborodaniel@gmail.com>
2024-06-15 23:20:41 +0100

    added get risk items

--- controllers/risk_item.js
+++ controllers/risk_item.js
@@ -1,5 +1,14 @@
 // router.post('/risk-items',
-    
+const getRiskItems = async (req, res) => {
+    try {
+      const riskItems = await RiskItem.find();
+      res.status(200).json(riskItems);
+    } catch (error) {
+      console.error('Error fetching risk items:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  };
+  
     const create_risk_item = async (req, res) => {
     try {
       const newRiskItem = new RiskItem(req.body);
