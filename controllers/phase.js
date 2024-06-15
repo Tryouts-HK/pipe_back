54de70885b26e9c6531b2d88e0aca85bb98bb561
Daniel Asaboro <asaborodaniel@gmail.com>
2024-06-15 23:21:33 +0100

    added getphases controller

--- controllers/phase.js
+++ controllers/phase.js
@@ -1,4 +1,15 @@
-// router.post('/phases', 
+// router.post('/phases', \\const getPhases = async (req, res) => {
+    const getPhases = async (req, res) => {
+        try {
+          const phases = await Phase.find();
+          res.status(200).json(phases);
+        } catch (error) {
+          console.error('Error fetching phases:', error);
+          res.status(500).json({ error: 'Internal Server Error' });
+        }
+      };
+      
+
    const addPhaseController = async (req, res) => {
     try {
       const newPhase = new Phase(req.body);
