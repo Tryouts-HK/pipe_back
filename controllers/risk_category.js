b0a64caf27763d35332a7628ec5eb4337a76a47a
Daniel Asaboro <asaborodaniel@gmail.com>
2024-06-15 23:20:13 +0100

    added getRisk category controller

--- controllers/risk_category.js
+++ controllers/risk_category.js
@@ -11,3 +11,13 @@
     }
   }
   
+  const getRiskCategories = async (req, res) => {
+    try {
+      const riskCategories = await RiskCategory.find();
+      res.status(200).json(riskCategories);
+    } catch (error) {
+      console.error('Error fetching risk categories:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  };
+  
