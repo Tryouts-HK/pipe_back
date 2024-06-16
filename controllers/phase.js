27177c4f8c3c6680a00523c90a722a5547f06851
Daniel Asaboro <asaborodaniel@gmail.com>
2024-06-16 23:28:47 +0100

    update phase controller

--- controllers/phase.js
+++ controllers/phase.js
@@ -1,24 +1,69 @@
-// router.post('/phases', \\const getPhases = async (req, res) => {
-    const getPhases = async (req, res) => {
-        try {
-          const phases = await Phase.find();
-          res.status(200).json(phases);
-        } catch (error) {
-          console.error('Error fetching phases:', error);
-          res.status(500).json({ error: 'Internal Server Error' });
-        }
-      };
-      
+const Phase = require('../models/Phase');
 
-   const addPhaseController = async (req, res) => {
-    try {
-      const newPhase = new Phase(req.body);
-      const savedPhase = await newPhase.save();
-  
-      res.status(201).json(savedPhase);
-    } catch (error) {
-      console.error('Error creating phase:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
+const getAllPhases = async (req, res) => {
+  try {
+    const phases = await Phase.find();
+    res.status(200).json(phases);
+  } catch (error) {
+    console.error('Error fetching phases:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
-  
+};
+
+const getPhaseById = async (req, res) => {
+  try {
+    const phase = await Phase.findById(req.params.id);
+    if (!phase) {
+      return res.status(404).json({ error: 'Phase not found' });
+    }
+    res.status(200).json(phase);
+  } catch (error) {
+    console.error('Error fetching phase:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+const addPhase = async (req, res) => {
+  try {
+    const newPhase = new Phase(req.body);
+    const savedPhase = await newPhase.save();
+    res.status(201).json(savedPhase);
+  } catch (error) {
+    console.error('Error creating phase:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+const updatePhase = async (req, res) => {
+  try {
+    const updatedPhase = await Phase.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!updatedPhase) {
+      return res.status(404).json({ error: 'Phase not found' });
+    }
+    res.status(200).json(updatedPhase);
+  } catch (error) {
+    console.error('Error updating phase:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+const deletePhase = async (req, res) => {
+  try {
+    const deletedPhase = await Phase.findByIdAndDelete(req.params.id);
+    if (!deletedPhase) {
+      return res.status(404).json({ error: 'Phase not found' });
+    }
+    res.status(200).json({ message: 'Phase deleted successfully' });
+  } catch (error) {
+    console.error('Error deleting phase:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+module.exports = {
+  getAllPhases,
+  getPhaseById,
+  addPhase,
+  updatePhase,
+  deletePhase,
+};
