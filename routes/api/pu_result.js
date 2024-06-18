b0d325da393270306a9140126f909ea10b15ddda
Daniel Asaboro <asaborodaniel@gmail.com>
2024-06-18 14:43:19 +0100

    added pu result routes

---
+++ routes/api/pu_result.js
@@ -0,0 +1,30 @@
+
+import express from 'express';
+import {
+  createPollingUnitResult,
+  getPollingUnitResultById,
+  getAllPollingUnitResults,
+  updatePollingUnitResult,
+  deletePollingUnitResult
+} from '../controllers/pollingUnitResults';
+
+const router = express.Router();
+
+// Route to create a new polling unit result
+router.post('/', createPollingUnitResult);
+
+
+// Route to get all polling unit results with pagination
+router.get('/', getAllPollingUnitResults);
+
+// Route to get a polling unit result by ID
+router.get('/:id', getPollingUnitResultById);
+
+
+// Route to update a polling unit result by ID
+router.put('/:id', updatePollingUnitResult);
+
+// Route to delete a polling unit result by ID
+router.delete('/:id', deletePollingUnitResult);
+
+export default router;
