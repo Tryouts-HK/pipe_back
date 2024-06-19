dca3ad0d2b6503ee83852782296ffd8ac5828169
Daniel Asaboro <asaborodaniel@gmail.com>
2024-06-19 07:48:26 +0100

    updated index to reflect changes to account routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import pollingUnitRoutes from "./polling_unit.js"
 import complaintRoutes from "./complaint.js"
 import puResultRoutes from './pu_result.js'
+import accountRoutes from './account.js'
 
 const router = Router();
 
@@ -13,6 +14,7 @@ const router = Router();
 router.use('/polling_units', pollingUnitRoutes);
 router.use('/complaint', complaintRoutes);
 router.use('/result', puResultRoutes);
+router.use('/account', accountRoutes);
 
 
 
