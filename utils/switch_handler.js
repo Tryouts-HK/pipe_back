15a833d22cb1187858066698050b1ec63c84b338
Daniel Asaboro <asaborodaniel@gmail.com>
2024-06-19 07:30:14 +0100

    added switch handler

--- utils/switch_handler.js
+++ utils/switch_handler.js
@@ -1,6 +1,7 @@
 
 import User from "../models/account/user.js";
-
+import Volunteer from "../models/account/volunteer.js"
+import Admin from "../models/account/admin.js"
 
 
 export const switchProfile = (role) => {
@@ -10,39 +11,14 @@ export const switchProfile = (role) => {
             case 'user':
                 profile = User;
                 break;
-            case 'doctor':
-                profile = Doctor;
+            case 'srv':
+                profile = Volunteer;
                 break;
-            case 'pharmacist':
-                profile = Pharmacist;
+            case 'admin':
+                profile = Admin;
                 break;
             default:
                 throw Error('Unsupported Role');
-        }
-        return profile;
-    } catch (error) {
-        throw error;
-    }
-}
-
-export const switchFacility = (facility) => {
-    let profile
-    try {
-        switch (facility) {
-            case 'hospital':
-                profile = Hospital;
-                break;
-            case 'clinic':
-                profile = Clinic;
-                break;
-            case 'pharmacy':
-                profile = Pharmacy;
-                break;
-            case 'ambulance':
-                profile = Ambulance;
-                break;
-            default:
-                throw Error('Unsupported Facility');
         }
         return profile;
     } catch (error) {
