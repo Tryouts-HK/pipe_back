33e861d5088c3ef8a5119ccf67e65fd73a8b1cb7
Daniel Asaboro <asaborodaniel@gmail.com>
2024-06-21 11:03:41 +0100

    expanded req handler

--- middlewares/req_error_handler.js
+++ middlewares/req_error_handler.js
@@ -1,22 +1,28 @@
-import  errorHandler from "../utils/error_handler.js";
+import errorHandler from "../utils/error_handler.js";
 
 const reqErrorHandler = async (err, req, res, next) => {
-    console.log('error middleware');
-    console.log(err);
-    if (err) {
-        try {
-            const cleanedError = errorHandler(err);
-            if (req.url.includes('/google/callback?')) {
-                res.redirect('/api/v1/auth/login?feedback=emailalreadyinuse');
-            } else {
-                res.status(400).json({ error: cleanedError });
-            }
-        } catch (error) {
-            console.log(error)
-            res.json({ error: "fatal error" });
+  console.log("error middleware");
+  console.log(err);
+  if (err) {
+    // current work around for passport jwt
+    if (err.message == "err is not defined") {
+      res
+        .status(401)
+        .json({ status: "error", message: "access token is invalid" });
+    } else {
+      try {
+        const cleanedError = errorHandler(err);
+        if (req.url.includes("/google/callback?")) {
+          res.redirect("/api/v1/auth/login?feedback=emailalreadyinuse");
+        } else {
+          res.status(400).json({ error: cleanedError });
         }
+      } catch (error) {
+        console.log(error);
+        res.json({ error: "fatal error" });
+      }
     }
-}
-
+  }
+};
 
 export default reqErrorHandler;
